916e8b6e426de03e77d18fbbf9e9f9716533300c
trclst <141440559+trclst@users.noreply.github.com>
2024-01-08 19:37:32 +0100

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt remove wpa* avahi* cups* Modem* packagekit* evolution-common gnome-music gnome-calendar gnome-contacts libebook* chrome* gnome-sushi gnome-games *switcheroo* *document* Network* *tracker* xit* geoclue* *printer* xserver-xorg* bluez* ghostscript* xserver-xephyr cheese* synaptic transmiss* simple-scan rhyth* gnome-sound* gnome-text* eog* realmd* sudo*
